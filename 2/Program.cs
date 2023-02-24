3ed9bce4150cf2e6b8d7514fcca6820fdf2cccf5
M9YNICE <M9YNICE@DESKTOP-CPPUUA1>
2023-02-24 21:04:37 +0300

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите числовую последовательность: ");
+try
+{
+    string seq = Console.ReadLine();
+    if (seq[0] < seq[1] && seq[1] < seq[2])
+        Console.WriteLine("Последовательность - возрастающая");
+    else
+        if (seq[0] > seq[1] && seq[1] > seq[2])
+        Console.WriteLine("Последовательность - убывающая");
+    else Console.WriteLine("Последовательность не является ни возрастающей ни убывающей");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
